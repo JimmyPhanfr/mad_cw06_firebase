ba1fc85d0a278e17898b9f7bc5837e612b86799e
JimmyPhanfr <JimmyPhanfr@gmail.com>
2025-04-06 21:57:42 -0400

    adds basic func to add list item, going to work on listview next

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,12 +36,151 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController detailsController = TextEditingController();
+  final TextEditingController dateController = TextEditingController();
+  final TextEditingController timeController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  void createTask(bool isCompleted, String date, String time, String name, String details) async {
+    await FirebaseFirestore.instance.collection('list').add({
+      "isCompleted" : isCompleted,
+      "date" : date,
+      "time" : time,
+      "name" : name,
+      "details" : details,
+      // to implement later
+      // "parent" : parent,
+      // "children" : children
     });
+  }
+
+Future<void> openAddTask(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            backgroundColor: Colors.white,
+            title: Text(
+              "Create New Task",
+              style: const TextStyle(color: Colors.black),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 12,
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: "Name",
+                    hintText: "Give the task a name",
+                    border: UnderlineInputBorder(),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        nameController.clear();
+                      },
+                      icon: const Icon(Icons.clear),
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: detailsController,
+                  decoration: InputDecoration(
+                    hintText: "Write a short description",
+                    border: UnderlineInputBorder(),
+                    labelText: "Details",
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        detailsController.clear();
+                      },
+                      icon: const Icon(Icons.clear),
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: dateController,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: "Date",
+                    hintText: "Set a date",
+                    border: UnderlineInputBorder(),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        _selectDate(context);
+                      },
+                      icon: const Icon(Icons.calendar_month_outlined),
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: timeController,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: "Time (optional)",
+                    hintText: "Set a time",
+                    border: UnderlineInputBorder(),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        _selectTime(context);
+                      },
+                      icon: const Icon(Icons.more_time_outlined),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("Cancel"),
+              ),
+              TextButton(
+                onPressed: () {
+                  createTask(
+                    false,
+                    dateController.text,
+                    timeController.text,
+                    nameController.text,
+                    detailsController.text,
+                  );
+                },
+                child: Text("Submit Item"),
+              ),
+            ],
+          ),
+    );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2101)
+      );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        dateController.text = DateFormat('MM-dd-yyyy').format(picked);
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+      initialEntryMode: TimePickerEntryMode.input,
+    );
+    if (picked != null) {
+      setState(() {
+        selectedTime = picked;
+        timeController.text = picked.format(context);
+      });
+    }
   }
 
   @override
@@ -53,17 +194,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          openAddTask(context);
+        }, 
+        tooltip: "create task",
         child: const Icon(Icons.add),
       ),
     );
